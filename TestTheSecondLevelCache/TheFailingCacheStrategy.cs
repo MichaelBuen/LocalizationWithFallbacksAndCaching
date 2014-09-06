7d22cb1882af8757565b446dcf0164201cafe4b6
Kel <michael.buen@ienablemuch.com>
2014-09-06 18:50:50 +0800

    Additional unit test

--- TestTheSecondLevelCache/TheFailingCacheStrategy.cs
+++ TestTheSecondLevelCache/TheFailingCacheStrategy.cs
@@ -104,7 +104,53 @@ namespace TestTheSecondLevelCache
 
         }
 
-  
+
+        [TestMethod]
+        public void Fail_Caching_NoPagingOfDependentTable_IfStale_WhenReferencedTableIsUpdatedOutside_AndQueryCacheNotEvicted_AndCacheRegionNotSpecificiedInQuery()
+        {
+
+
+
+            var sf = Common.BuildSessionFactory();
+
+
+
+
+
+
+            using (var session = sf.OpenSession())
+            using (var tx = session.BeginTransaction())
+            {
+                Console.WriteLine("Query 1");
+                var list = session.Query<Order>().OrderBy(x => x.Person.FirstName).Cacheable().ToList();
+            }
+
+            using (var session = sf.OpenStatelessSession())
+            using (var tx = session.BeginTransaction())
+            {
+                Console.WriteLine("Stateless update");
+                var p = session.Get<Person>(1);
+                p.FirstName = "ZX-" + p.FirstName;
+                session.Update(p);
+                tx.Commit();
+
+
+
+            }
+
+            using (var session = sf.OpenSession())
+            using (var tx = session.BeginTransaction())
+            {
+                Console.WriteLine("Query 2");
+                // this Order query is oblivous to changes on Person entity, hence this will not requery the Order with new sorting, and then this will return stale cache
+                var list = session.Query<Order>().OrderBy(x => x.Person.FirstName).Cacheable().ToList();
+                Assert.AreEqual("Paul", list[0].Person.FirstName);
+                Assert.AreEqual("ZX-John", list[1].Person.FirstName);
+            }
+
+        }
+
+
 
     }
 }
